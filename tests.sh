a4d7b503517dce77cfef0f0770b0967624396403
james miller <jamesstewartmiller@gmail.com>
2022-10-17 16:13:23 +0100

    edited tests.sh to give meaningful exit code

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+exec 3>&1
 ansible-playbook django_messages/testapp/ansible_setup_test.yml \
-&& python -m pytest \
-&& ansible-playbook django_messages/testapp/ansible_teardown_test.yml
+&& EXITCODE=$(python -m pytest | tee >(grep -Po '\d+\sfailed' | grep -Po '\d+') >&3) \
+&& ansible-playbook django_messages/testapp/ansible_teardown_test.yml \
+&& exit ${EXITCODE}
