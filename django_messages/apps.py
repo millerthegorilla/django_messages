0296f239673d182cffeca4fa4856c71974fdc3f6
james miller <jamesstewartmiller@gmail.com>
2022-10-22 21:39:43 +0100

    experimenting with apps.ready

--- django_messages/apps.py
+++ django_messages/apps.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from crispy_forms import templates as crispy_templates
 from crispy_bootstrap5 import templates as bs5_templates
 
-APPS = [
+my_apps = [
     {"name": "django_q", "templates": ""},
     {"name": "crispy_forms", "templates": crispy_templates},
     {"name": "crispy_forms_tags", "templates": ""},
@@ -21,12 +21,14 @@ class DjangoMessagesConfig(AppConfig):
     name = "django_messages"
 
     def ready(self) -> None:
-        for app in APPS:
+        global my_apps
+        for app in my_apps:
             if app["name"] not in settings.INSTALLED_APPS:
-                theapp = importlib.import_module(app["name"])
-                from theapp import apps as theapp_apps
-
-                theapp_apps.ready()
+                theapp = importlib.import_module(app["name"] + ".apps")
+                try:
+                    my_apps += theapp.my_apps
+                except AttributeError:
+                    pass
                 settings.INSTALLED_APPS += (app["name"],)
                 apps.app_configs = OrderedDict()
                 apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
