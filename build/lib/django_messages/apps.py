65326e36fbabadab1b3ba2260864f834917d085d
james miller <jamesstewartmiller@gmail.com>
2022-10-23 15:09:35 +0100

    experimenting with apps.ready

--- build/lib/django_messages/apps.py
+++ build/lib/django_messages/apps.py
@@ -34,8 +34,7 @@ class DjangoMessagesConfig(AppConfig):
                     )
                 settings.STATICFILES_DIRS += [
                     os.path.abspath(
-                        importlib.import_module(app["name"]).__path__.path[0]
-                        + "/static/"
+                        importlib.import_module(app["name"]).__path__[0] + "/static/"
                     )
                 ]
                 try:
