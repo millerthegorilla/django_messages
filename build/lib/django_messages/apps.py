9abc041e839f9971cab930becad17d6e24c8dbd7
james miller <jamesstewartmiller@gmail.com>
2022-10-23 21:54:45 +0100

    experimenting with apps.ready

--- build/lib/django_messages/apps.py
+++ build/lib/django_messages/apps.py
@@ -22,29 +22,48 @@ class DjangoMessagesConfig(AppConfig):
 
     def ready(self) -> None:
         global my_apps
-        # sdir = False
+        try:
+            if not settings.TOPLEVELCONFIG:
+                settings.TOPLEVELCONFIG = self.__name__
+                self.populate_my_apps()
+                self.install_apps()
+        except AttributeError:
+            settings.TOPLEVELCONFIG = self.__name__
+            self.populate_my_apps()
+            self.install_apps()
+
+    def populate_my_apps(self):
+        global my_apps
+        for app in my_apps:
+            try:
+                theapp = importlib.import_module(app["name"] + ".apps")
+                my_apps += [app for app in theapp.my_apps if app not in my_apps]
+                app["setup"] = theapp.setup_apps
+            except (ModuleNotFoundError, AttributeError):
+                pass
+
+    def install_apps(self):
         for app in my_apps:
             if app["name"] not in settings.INSTALLED_APPS:
                 settings.INSTALLED_APPS += (app["name"],)
-                # apps.app_configs = OrderedDict()
-                # apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
-                # apps.clear_cache()
-                # apps.populate(settings.INSTALLED_APPS)
-                # if app["templates"] != "":
-                #     settings.TEMPLATES[0]["DIRS"].append(
-                #         os.path.abspath(app["templates"].__path__._path[0])
-                #     )
-        #         static = os.path.abspath(
-        #             importlib.import_module(app["name"]).__path__[0] + "/static/"
-        #         )
-        #         if os.path.isdir(static):
-        #             sdir = True
-        #             settings.STATICFILES_DIRS += [static]
-        #         # try:
-        #         #     theapp = importlib.import_module(app["name"] + ".apps")
-        #         #     my_apps += theapp.my_apps
-        #         #     theapp.setup_apps()
-        #         # except (ModuleNotFoundError, AttributeError):
-        #         #     pass
+                apps.app_configs = OrderedDict()
+                apps.apps_ready = apps.models_ready = apps.loading = apps.ready = False
+                apps.clear_cache()
+                apps.populate(settings.INSTALLED_APPS)
+                if app["templates"] != "":
+                    settings.TEMPLATES[0]["DIRS"].append(
+                        os.path.abspath(app["templates"].__path__._path[0])
+                    )
+                if "setup" in app:
+                    app["setup"]()
+        # static = os.path.abspath(
+        #     importlib.import_module(app["name"]).__path__[0] + "/static/"
+        # )
+        # if os.path.isdir(static):
+        #     sdir = True
+        #     settings.STATICFILES_DIRS += [static]
+
         # if sdir:
         #     call_command("collectstatic", verbosity=0, interactive=False)
+
+    # setup_apps()
